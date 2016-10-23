3400fe6ed377af750b66997ec0b3be72937c5053
Daniel <daniel.gilljam@gmail.com>
2016-10-23 23:20:28 +0200

    comment

--- MyCommunity.DataLayer/Infrastructure/IDatabaseManager.cs
+++ MyCommunity.DataLayer/Infrastructure/IDatabaseManager.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyCommunity.DataLayer.Infrastructure
 {
+    // Interface that initialize instances of our dbcontext class
     public interface IDatabaseManager : IDisposable
     {
         DatabaseContext Init();
